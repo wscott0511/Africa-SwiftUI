53e3c03fa6a370e36154098c7e7f43cbab9a21b4
wscott0511 <57239286+wscott0511@users.noreply.github.com>
2020-11-30 11:26:11 -0800

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ryan Scott on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
